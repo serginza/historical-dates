bb12bf94e51dcd03ffd97bd32e1b738e5f4e9111
Sergey <polikarpov-sereg@mail.ru>
2025-04-09 20:59:21 +0300

    fix: fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
